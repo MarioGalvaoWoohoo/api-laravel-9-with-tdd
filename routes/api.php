1c18e24fb8b9f788c518cffad726c6d54eb93ea3
MarioGalvaoWoohoo <65731257+MarioGalvaoWoohoo@users.noreply.github.com>
2023-04-28 15:58:46 -0300

    implementação do jwt token

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,13 @@ use App\Http\Controllers\Api\{
 };
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('/users', UserController::class);
+// Route::apiResource('/users', UserController::class);
+
+Route::prefix('/v1')->group(function () {
+    Route::middleware(['jwt.verify'])->group(function () {
+        Route::get('/users', [UserController::class, 'listAll'])->name('users.listAll');
+    });
+});
 // Route::delete('/users/{email}', [UserController::class, 'destroy']);
 // Route::put('/users/{email}', [UserController::class, 'update']);
 // Route::get('/users/{email}', [UserController::class, 'show']);
