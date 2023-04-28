1dcdd69f99efa2610c122a3a795282ca19ce80a8
MarioGalvaoWoohoo <65731257+MarioGalvaoWoohoo@users.noreply.github.com>
2023-04-28 16:12:26 -0300

    Descomentado linha de router users

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Api\{
 };
 use Illuminate\Support\Facades\Route;
 
-// Route::apiResource('/users', UserController::class);
+Route::apiResource('/users', UserController::class);
 
 Route::prefix('/v1')->group(function () {
     Route::middleware(['jwt.verify'])->group(function () {
