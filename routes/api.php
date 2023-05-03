4dd0fb0959773e5f79415ca7c9eab228ad58622d
MarioGalvaoWoohoo <65731257+MarioGalvaoWoohoo@users.noreply.github.com>
2023-05-03 11:24:05 -0300

    Criada a migrate message e rotas

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\Api\{
-    UserController
+    UserController,
+    MessageController
 };
+
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('/users', UserController::class);
@@ -10,6 +12,17 @@ Route::apiResource('/users', UserController::class);
 Route::prefix('/v1')->group(function () {
     Route::middleware(['jwt.verify'])->group(function () {
         Route::get('/users', [UserController::class, 'listAll'])->name('users.listAll');
+
+        Route::get('/messages', [MessageController::class, 'listAll'])->name('messages.listAll');
+        Route::post('/message', [MessageController::class, 'store'])->name('messages.store');
+        Route::get('/message/{id}', [MessageController::class, 'show'])->name('messages.show');
+        Route::put('/message/{id}', [MessageController::class, 'update'])->name('messages.update');
+        Route::delete('/message/{id}', [MessageController::class, 'destroy'])->name('messages.destroy');
+        Route::get('/messagesIsActive', [MessageController::class, 'messagesIsActive'])->name('messages.messageIsActive');
+        Route::get('/messagesOnTimeIsActive', [MessageController::class, 'messagesOnTimeIsActive'])->name('messages.messageOnTimeIsActive');
+        Route::get('/unreadMessages', [MessageController::class, 'unreadMessages'])->name('messages.unreadMessages');
+        Route::put('/prioritizeMessage', [MessageController::class, 'prioritizeMessage'])->name('messages.prioritizeMessage');
+        Route::delete('/deprioritizeMessage', [MessageController::class, 'deprioritizeMessage'])->name('messages.deprioritizeMessage');
     });
 });
 // Route::delete('/users/{email}', [UserController::class, 'destroy']);
